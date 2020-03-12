68748861ff531266c908e3b118e69d596e837f89
Augustin Goudet <augustin.goudet@gmail.com>
2020-03-12 17:54:40 +0000

    Modelling

--- ml_repo.py
+++ ml_repo.py
@@ -171,7 +171,7 @@ def scores(model,X_train,X_val,y_train,y_val):
     val_prob = model.predict_proba(X_val)[:,1]
     train = roc_auc_score(y_train,train_prob)
     val = roc_auc_score(y_val,val_prob)
-    print('train:',round(train,2),'test:',round(val,2))
+    print('train accuracy:',round(train,2),'val/test accuracy:',round(val,2))
     
 #===========================================================================================================================================================
 def annot(fpr,tpr,thr):
